23721cb0f6ec69302299f6eb9e8fbdbbc1c3b949
simpzan <simpzan@gmail.com>
2018-03-05 11:09:12 +0800

    Initial Commit

---
+++ NAT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NAT
+//
+//  Created by simpzan on 05/03/2018.
+//  Copyright © 2018 simpzan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NAT/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NAT
+//
+//  Created by simpzan on 05/03/2018.
+//  Copyright © 2018 simpzan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
