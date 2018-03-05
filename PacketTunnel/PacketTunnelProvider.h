28767171d3ed11b2d22108147da2d9aad9dbefe2
simpzan <simpzan@gmail.com>
2018-03-05 11:34:58 +0800

    generate PacketTunnel target from template.

---
+++ PacketTunnel/PacketTunnelProvider.h
@@ -0,0 +1,13 @@
+//
+//  PacketTunnelProvider.h
+//  PacketTunnel
+//
+//  Created by simpzan on 05/03/2018.
+//  Copyright © 2018 simpzan. All rights reserved.
+//
+
+@import NetworkExtension;
+
+@interface PacketTunnelProvider : NEPacketTunnelProvider
+
+@end
