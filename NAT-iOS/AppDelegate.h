ae885ff03dafdc3f757d271ba3b5ba5fe8894e2d
simpzan <simpzan@gmail.com>
2018-03-12 21:37:41 +0800

    add iOS targets.

---
+++ NAT-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NAT-iOS
+//
+//  Created by simpzan on 12/03/2018.
+//  Copyright © 2018 simpzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NAT-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NAT-iOS
+//
+//  Created by simpzan on 12/03/2018.
+//  Copyright © 2018 simpzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
